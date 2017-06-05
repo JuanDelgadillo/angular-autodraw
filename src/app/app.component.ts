0c5c47ede3b697caffb60f0d7c9619ab6aacf03d
Juan Delgadillo <j.mnuelh@gmail.com>
2017-06-05 12:38:35 -0600

    Add AutoDraw's main logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,181 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { AutoDrawService } from './services';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, OnDestroy {
+
+  constructor (
+    private autoDrawService: AutoDrawService
+  ) {}
+
+  @ViewChild('canvas') canvas;
+
+  drawSuggestions: Array<object>;
+
+  canvasMouseMoveEvent: Subscription;
+  canvasMouseDownEvent: Subscription;
+  canvasMouseOutEvent:  Subscription;
+  canvasMouseUpEvent:   Subscription;
+
+  previousXAxis: number = 0;
+  previousYAxis: number = 0;
+  currentXAxis:  number = 0;
+  currentYAxis:  number = 0;
+
+  context;
+  pressedAt: number;
+  pressing:  boolean = false;
+  currentShape: Array<number[]>;
+  shapes: Array<Array<number[]>> = [];
+  intervalLastPosition: number[] = [-1, -1];
+
+  ngOnInit () {
+    this.autoDrawService.loadStencils();
+    this.context = this.canvas.nativeElement.getContext('2d');
+
+    this.canvasMouseMoveEvent = Observable
+      .fromEvent(this.canvas.nativeElement, 'mousemove')
+      .subscribe((event: MouseEvent) => {
+        this.draw(event);
+      });
+
+    this.canvasMouseDownEvent = Observable
+      .fromEvent(this.canvas.nativeElement, 'mousedown')
+      .subscribe((event: MouseEvent) => {
+        this.draw(event);
+      });
+
+    this.canvasMouseUpEvent = Observable
+      .fromEvent(this.canvas.nativeElement, 'mouseup')
+      .subscribe((event: MouseEvent) => {
+        this.draw(event);
+      });
+
+    this.canvasMouseOutEvent = Observable
+      .fromEvent(this.canvas.nativeElement, 'mouseout')
+      .subscribe((event: MouseEvent) => {
+        this.draw(event);
+      });
+  }
+
+  ngOnDestroy () {
+    this.canvasMouseUpEvent.unsubscribe();
+    this.canvasMouseOutEvent.unsubscribe();
+    this.canvasMouseMoveEvent.unsubscribe();
+    this.canvasMouseDownEvent.unsubscribe();
+  }
+
+  eraseCanvas () {
+    this.shapes = [];
+    this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  }
+
+  prepareNewShape () {
+    this.currentShape = [
+      [], // X coordinates
+      [], // Y coordinates
+      []  // Times
+    ];
+  }
+
+  storeCoordinates() {
+    if (this.intervalLastPosition[0] !== this.previousXAxis && this.intervalLastPosition[1] !== this.previousYAxis) {
+      this.intervalLastPosition = [this.previousXAxis, this.previousYAxis];
+      this.currentShape = [
+        [...this.currentShape[0], this.previousXAxis],
+        [...this.currentShape[1], this.previousYAxis],
+        [...this.currentShape[2], Date.now() - this.pressedAt]
+      ];
+    }
+  }
+
+  onDrawingMouseDown (mouseEvent: MouseEvent) {
+    let highlightStartPoint, drawColorStartingPoint = 'black';
+
+    this.previousXAxis = this.currentXAxis;
+    this.previousYAxis = this.currentYAxis;
+    this.currentXAxis = mouseEvent.clientX - this.canvas.nativeElement.offsetLeft;
+    this.currentYAxis = mouseEvent.clientY - this.canvas.nativeElement.offsetTop;
+
+    this.pressing = true;
+    this.pressedAt = Date.now();
+    highlightStartPoint = true;
+
+    this.prepareNewShape();
+
+    if (highlightStartPoint) {
+      this.context.beginPath();
+      this.context.fillStyle = drawColorStartingPoint;
+      this.context.fillRect(this.currentXAxis, this.currentYAxis, 2, 2);
+      this.context.closePath();
+      highlightStartPoint = false;
+    }
+
+    // Stores coordinates every 9ms
+    return window.setInterval(() => this.storeCoordinates(), 9);
+  }
+
+  onDrawingMouseMove (mouseEvent: MouseEvent) {
+    let drawStroke = 8, drawColor  = 'black';
+
+    this.previousXAxis = this.currentXAxis;
+    this.previousYAxis = this.currentYAxis;
+    this.currentXAxis = mouseEvent.clientX - this.canvas.nativeElement.offsetLeft;
+    this.currentYAxis = mouseEvent.clientY - this.canvas.nativeElement.offsetTop;
+
+    this.context.beginPath();
+    this.context.moveTo(this.previousXAxis, this.previousYAxis);
+    this.context.lineTo(this.currentXAxis, this.currentYAxis);
+    this.context.strokeStyle = drawColor;
+    this.context.fillStyle = drawColor;
+    this.context.lineCap = 'round';
+    this.context.lineJoin = 'round';
+    this.context.lineWidth = drawStroke;
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  draw(mouseEvent: MouseEvent) {
+    let storeCoordinateInterval;
+
+    if (mouseEvent.type === 'mousedown') {
+      storeCoordinateInterval = this.onDrawingMouseDown(mouseEvent);
+    }
+
+    if (mouseEvent.type === 'mouseup' || this.pressing && mouseEvent.type === 'mouseout') {
+      this.pressing = false;
+      clearInterval(storeCoordinateInterval);
+      this.commitCurrentShape();
+    }
+
+    if (mouseEvent.type === 'mousemove' && this.pressing) {
+      this.onDrawingMouseMove(mouseEvent);
+    }
+  }
+
+  commitCurrentShape() {
+    this.shapes.push(this.currentShape);
+    let drawOptions = {
+      canvasWidth: this.canvas.nativeElement.width,
+      canvasHeight: this.canvas.nativeElement.height
+    };
+
+    this.autoDrawService.drawSuggestions(this.shapes, drawOptions)
+      .subscribe(suggestions => this.drawSuggestions = suggestions);
+  }
+
+  pickSuggestion(source: string) {
+    this.eraseCanvas();
+    let image = new Image();
+    image.onload = () => this.context.drawImage(image, 0, 0);
+    image.src = source;
+  }
+
 }
