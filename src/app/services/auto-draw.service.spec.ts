87e9da0347ebee1a417cdd8f96a81e2f5d60294b
Juan Delgadillo <j.mnuelh@gmail.com>
2017-06-09 10:30:17 -0600

    Add necessary dependencies in AutoDrawService and AppComponent's tests

--- src/app/services/auto-draw.service.spec.ts
+++ src/app/services/auto-draw.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { AutoDrawService } from './auto-draw.service';
 
 describe('AutoDrawService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AutoDrawService]
+      providers: [AutoDrawService],
+      imports: [
+        HttpModule
+      ]
     });
   });
 
